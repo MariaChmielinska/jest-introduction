e9cd5409265c859220a24efcfd65d0127645d774
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-10-08 22:00:06 +0200

    falta 2B

--- scoring-words.test.js
+++ scoring-words.test.js
@@ -1,1 +1,26 @@
 const { high } = require('./scoring-words'); 
+
+test('debería devolver taxi', () => {
+    expect(high('man i need a taxi up to ubud')).toBe('taxi'); 
+});
+
+
+test('debería devolver volcano', () => {
+    expect(high('what time are we climbing up the volcano')).toBe('volcano'); 
+});
+
+
+test('debería devolver semynak', () => {
+    expect(high('take me to semynak')).toBe('semynak'); 
+});
+
+test('deberia devolver aa', () => {
+    expect(high('aa b')).toBe('aa'); 
+});
+
+
+it('should return a word with all the vowels in case there is one present', () => {
+    const words = "I think education is the most valuable investment in a modern state";
+    const maxPointsWord = high(words);
+    expect(maxPointsWord).toBe("education");
+})
